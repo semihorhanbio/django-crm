de70733f3ceb477be7e02803595da33828afbde3
Semih Orhan <sorhan3.14@gmail.com>
2023-07-18 12:18:48 +0300

    create lead submit form

--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
 from .models import Lead
+from .forms import LeadModelForm
 
 def lead_list(request):
     context = {'leads': Lead.objects.all()}
@@ -9,3 +9,13 @@ def lead_list(request):
 def lead_detail(request, pk):
     context = {'lead': Lead.objects.get(id=pk)}
     return render(request, 'lead_detail.html', context)
+
+def lead_create(request):
+    form = LeadModelForm()
+    if request.method == 'POST':
+        form = LeadModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/leads')
+    context = {'form': form}
+    return render(request, 'lead_create.html', context)
