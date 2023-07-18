6c5dc13e7e24f2f50ac3e32ca5b940cf208ef322
Semih Orhan <sorhan3.14@gmail.com>
2023-07-18 16:06:36 +0300

    add statics folder

--- djangocrm/settings.py
+++ djangocrm/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+STATIC_ROOT = 'static_root'
 
 AUTH_USER_MODEL = 'leads.User'
 
--- djangocrm/urls.py
+++ djangocrm/urls.py
@@ -1,4 +1,5 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from leads.views import landing_page, LandingPageView
@@ -8,3 +9,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('leads/', include('leads.urls', namespace='leads')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
